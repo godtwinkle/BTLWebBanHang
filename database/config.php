4b1a09e9bbd7b277fe0371bf4659ebf472bb3420
God Twinkle <tnl04122001@gmail.com>
2022-09-28 18:27:49 +0700

    lan 1

---
+++ database/config.php
@@ -0,0 +1,6 @@
+<?php
+define('HOST','localhost:3307'); 
+define('DATABASE','webbanhang');
+define('USERNAME','root');
+define('PASSWORD','');
+define('PRIVATE_KEY','sdasdadasdasdadqwiy832!@#$%^dwe');
